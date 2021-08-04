a501a52636ef33bc757ec8f8c7177d2b05b839f3
Duy Nguyen <garlicdevs@gmail.com>
2021-08-04 14:52:22 +1000

    Training 20 epochs

--- configs/smoking/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_smoking.py
+++ configs/smoking/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_smoking.py
@@ -1,5 +1,5 @@
 # The new config inherits a base config to highlight the necessary modification
-_base_ = '/home/thanh/work/scanner_dl/mmdetection/configs/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
+_base_ = '../mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
 
 # We also need to change the num_classes in head to match the dataset's annotation
 model = dict(
@@ -12,17 +12,55 @@ dataset_type = 'COCODataset'
 classes = ('Cigarette',)
 data = dict(
     train=dict(
-        img_prefix='/home/thanh/work/scanner_dl/mmdetection/data/smoking/train/',
+        img_prefix='data/smoking/train/',
         classes=classes,
-        ann_file='/home/thanh/work/scanner_dl/mmdetection/data/smoking/train/train_coco.json'),
+        ann_file='data/smoking/train/train_coco.json'),
     val=dict(
-        img_prefix='/home/thanh/work/scanner_dl/mmdetection/data/smoking/val/',
+        img_prefix='data/smoking/val/',
         classes=classes,
-        ann_file='/home/thanh/work/scanner_dl/mmdetection/data/smoking/val/val_coco.json'),
+        ann_file='data/smoking/val/val_coco.json'),
     test=dict(
-        img_prefix='/home/thanh/work/scanner_dl/mmdetection/data/smoking/test/',
+        img_prefix='data/smoking/test/',
         classes=classes,
-        ann_file='/home/thanh/work/scanner_dl/mmdetection/data/smoking/test/test_coco.json'))
+        ann_file='data/smoking/test/test_coco.json'))
+
+img_norm_cfg = dict(
+    mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='LoadAnnotations',
+        with_bbox=True,
+        with_mask=True,
+        poly2mask=False),
+    dict(
+        type='Resize',
+        img_scale=[(512, 512)],
+        multiscale_mode='value',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(512, 512),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
 
 # We can use the pre-trained Mask RCNN model to obtain higher performance
-load_from = '../checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
+load_from = 'checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
